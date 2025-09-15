f18ffc3c41d96ac21825420639f794e603857725
webspind <webspind@gmail.com>
2025-09-15 15:55:11 +0200

    redploy

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 // You can alternatively inject these via env and hydrate on the page.
 window.STRIPE_PUBLISHABLE_KEY = "pk_test_51S4pQrEvo3YAiMiXlE2UxJh1TZ6P5JKAdcdZ5nIhAnJXdW7MbQBKkbU2vLSNzLqis8pw573QV1reRMYYw5bNdzKc00xwzESZDy";
 // Example: map button handlers to these IDs
-window.PRICE_10 = window.PRICE_10 || ""; // e.g. price_...
+window.PRICE_10 = window.PRICE_10 || "price_1S7cWAEvo3YAiMiXJWel7ohm";
+window.PRICE_30 = window.PRICE_30 || "price_1S7cWAEvo3YAiMiXOnlf9hdV";
 window.PRICE_40 = window.PRICE_40 || "";
 window.PRICE_120 = window.PRICE_120 || "";
 window.PRICE_400 = window.PRICE_400 || "";
